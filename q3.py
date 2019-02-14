bdec5f51c9fe5c23cde2d5ad73e77c5e5d348a2e
Dominic Heaton <dhh1g15@soton.ac.uk>
2019-02-14 09:49:18 +0000

    Add freq analysis for Q3

--- q3.py
+++ q3.py
@@ -14,6 +14,45 @@ def loadFile():
     # print(ciphertext)
     return ciphertext
 
+def letterFrequency(ct):
+    ct = ct.upper() #make all upper case
+    freqAlphabet = {'A' : ct.count('A'), 'B' : ct.count('B'), 'C' : ct.count('C'),
+                    'D' : ct.count('D'), 'E' : ct.count('E'), 'F' : ct.count('F'),
+                    'G' : ct.count('G'), 'H' : ct.count('H'), 'I' : ct.count('I'),
+                    'J' : ct.count('J'), 'K' : ct.count('K'), 'L' : ct.count('L'),
+                    'M' : ct.count('M'), 'N' : ct.count('N'), 'O' : ct.count('O'),
+                    'P' : ct.count('P'), 'Q' : ct.count('Q'), 'R' : ct.count('R'),
+                    'S' : ct.count('S'), 'T' : ct.count('T'), 'U' : ct.count('U'),
+                    'V' : ct.count('V'), 'W' : ct.count('W'), 'X' : ct.count('X'),
+                    'Y' : ct.count('Y'), 'Z' : ct.count('Z')}
+    return freqAlphabet
+
+def englishFrequency():
+    freqEnglish = {'A': 8.17, 'B': 1.29, 'C': 2.78, 'D': 4.25,'E': 12.70,
+                'F': 2.23, 'G': 2.02, 'H': 6.09, 'I': 6.97, 'J': 0.15,
+                'K': 0.77, 'L': 4.03,  'M': 2.41, 'N': 6.75, 'O': 7.51,
+                'P': 1.93, 'Q': 0.10, 'R': 5.99, 'S': 6.33, 'T': 9.06,
+                'U': 2.76, 'V': 0.98, 'W': 2.36, 'X': 0.15, 'Y': 1.97,
+                'Z': 0.07}
+    return freqEnglish
+
 ciphertext = loadFile()
 print('\nStarting Decipher Attempt...\n')
 print(' >Ciphertext: \n' + ciphertext + '\n')
+
+#Frequency analysis
+ciphertextFrequency = letterFrequency(ciphertext)
+englishFrequency = englishFrequency()
+
+#graphical representation of the analysis
+x1, y1 = zip(*ciphertextFrequency.items())
+xe, ye = zip(*englishFrequency.items())
+plt.plot(x1, y1, xe, ye)
+plt.title('Comparison of freqency of letters in CT (blue) and English (orange)')
+plt.xlabel('letter')
+plt.ylabel('frequency')
+plt.show(block=False)
+
+
+#leave as final line
+plt.show()
