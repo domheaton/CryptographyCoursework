449b88a6c03708f2781dcee2739fe1fadf114ff4
Dominic Heaton <dhh1g15@soton.ac.uk>
2019-02-14 09:36:49 +0000

    q2 and q3 python scripts

---
+++ q3.py
@@ -0,0 +1,19 @@
+# Author: Dominic Heaton
+# Solution Script for Q3
+#################################################################################
+from langdetect import detect
+from guess_language import guess_language
+from termcolor import colored
+import re
+import matplotlib.pylab as plt
+
+def loadFile():
+    open_file = open("q3.txt", "r")
+    ciphertext = open_file.read().rstrip('\n')
+    open_file.close()
+    # print(ciphertext)
+    return ciphertext
+
+ciphertext = loadFile()
+print('\nStarting Decipher Attempt...\n')
+print(' >Ciphertext: \n' + ciphertext + '\n')
